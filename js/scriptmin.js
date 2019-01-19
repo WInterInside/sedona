274ad55c7bd9b7538a29880074f77ad74c1a9474
Cruel Winter <de_animator@mail.ru>
2019-01-19 19:30:27 +0300

    Защита. Попытка 3
    отработаны последние замечания по верстке. подключены скрипты

---
+++ js/scriptmin.js
@@ -0,0 +1,1 @@
+var search=document.querySelector("a.modal-search");var popup=document.querySelector(".modal");search.addEventListener("click",function(evn){evn.preventDefault();popup.classList.toggle("modal-show");;});
